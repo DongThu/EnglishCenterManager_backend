d053cbb0dbe8e5c0671896dfe3521f70c2bf41d6
DongThu <thub1910458@student.ctu.edu.vn>
2023-11-28 15:36:04 +0700

    update

--- src/main/java/com/example/EnglishCenterManager_backend/exam/ExamRequest.java
+++ src/main/java/com/example/EnglishCenterManager_backend/exam/ExamRequest.java
@@ -1,5 +1,6 @@
 package com.example.EnglishCenterManager_backend.exam;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class ExamRequest {
     
+    @NotNull
     private Integer courseId;
 
+    @NotNull
     private Integer quizId;
 }
